9312c8221b8f70beb45ed3e6eec41f94282a1eaf
Karinna Iniguez <karinnainiguez@gmail.com>
2018-02-26 10:31:48 -0800

    Create tests for Card methods.  Passing tests

--- lib/card.rb
+++ lib/card.rb
@@ -7,12 +7,28 @@ class Card
   def initialize(value, suit)
     @value = value
     @suit = suit
-    
-
+    valid_suits = [:hearts, :spades, :clubs, :diamonds]
+    valid_values = (1..13)
+    unless valid_suits.include?(suit) && valid_values.include?(value)
+      raise ArgumentError.new
+    end
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    return "#{name} of #{suit.to_s}"
+  end
+
+  def name
+    case value
+    when 11
+      name = "Jack"
+    when 12
+      name = "Queen"
+    when 13
+      name = "King"
+    else
+      name = value
+    end
   end
 
 end
--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -27,11 +27,17 @@ describe Card do
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+      card1 = Card.new(2,:diamonds)
+      result = card1.to_s
+      result.must_equal "2 of diamonds"
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
       # Test to ensure that to_s works for cards values 1, and 11-13
       # For example: "Queen of hearts"
+      card2 = Card.new(13,:hearts)
+      result = card2.to_s
+      result.must_equal "King of hearts"
     end
   end
 
@@ -39,11 +45,16 @@ describe Card do
 
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
+      card3 = Card.new(8, :clubs)
+      result = card3.value
+      result.must_equal 8
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
-
+      card4 = Card.new(5, :spades)
+      result = card4.suit
+      result.must_equal :spades
     end
   end
 
