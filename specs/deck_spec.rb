a4d7f1c915014aeac2b771a4c148a76ca601fc6b
Karinna Iniguez <karinnainiguez@gmail.com>
2018-02-26 10:44:20 -0800

    Add tests for Deck methods

--- specs/deck_spec.rb
+++ specs/deck_spec.rb
@@ -8,4 +8,60 @@ describe Deck do
     # 1.  Expected Behavior
     # 2.  Edge cases
 
+  describe 'Deck instance' do
+    it "can be created" do
+      deck1 = Deck.new
+    end
+
+    it "has 52 Card objects as attributes" do
+      deck2 = Deck.new
+      result = deck2.cards
+      result.must_be_kind_of Array
+      result.length.must_equal 52
+      result[5].must_be_kind_of Card
+    end
+  end
+
+  describe '#shuffle' do
+    it 'can be called' do
+      deck3 = Deck.new
+      deck3.shuffle
+    end
+  end
+
+  describe '#draw' do
+    it 'returns card' do
+      deck4 = Deck.new
+      result = deck4.draw
+      result.must_be_kind_of Card
+    end
+
+    it 'removes Card from Deck' do
+      deck5 = Deck.new
+      deck5.draw
+      deck5.draw
+      deck5.draw
+      deck5.count.must_equal 49
+    end
+  end
+
+  describe '#count' do
+
+    it 'returns the number of cards in Deck' do
+      deck6 = Deck.new
+      result = deck6.cards
+      result.must_equal 52
+    end
+
+
+    it 'returns updated number of cards after draw' do
+      deck7 = Deck.new
+      deck7.draw
+      deck7.draw
+      result = deck7.cards
+      result.must_equal 50
+    end
+  end
+
+
 end
